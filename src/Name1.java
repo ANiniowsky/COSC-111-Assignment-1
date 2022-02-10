b6bda6a9242add20ada2ff0a224acb55983f8260
ANiniowsky <aniniows@emich.edu>
2022-02-10 18:32:36 -0500

    Adding initial 3 files for project 1, which include assignment 1

---
+++ src/Name1.java
@@ -0,0 +1,16 @@
+/*
+Alexander Niniowsky
+ID: 49103
+COSC 111
+Assignment 1A
+Description: This program will print out my name, student ID, and address on three seperate
+lines. It will use the Java command System.out.println()
+*/
+
+public class Name1 {
+    public static void main(String[] args){
+        System.out.println("Alexander Niniowsky");
+        System.out.println("ID E01026688");
+        System.out.println("1321 Highview Street");
+    }
+}
---
+++ src/Name2.java
@@ -0,0 +1,16 @@
+/*
+Alexander Niniowsky
+ID: 49103
+COSC 111
+Assignment 1A
+Description: This program will print out my name, student ID, and address on three seperate
+lines. It will use the Java command System.out.print()
+*/
+
+public class Name2 {
+    public static void main(String[] args){
+        System.out.print("\nAlexander Niniowsky");
+        System.out.print("\nID E01026688");
+        System.out.print("\n1321 Highview Street");
+    }
+}
